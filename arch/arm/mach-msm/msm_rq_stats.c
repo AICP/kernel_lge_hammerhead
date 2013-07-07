81e4b1002542bf6d3b1de27004c2da924ad882d7
maxwen <max.weninger@gmail.com>
2013-07-07 16:56:52 +0200

    msm_rq_stats: fix race between stats and cpuquiet
    Change-Id: I86adfed146ca0588793959e2c1d728f0763b2ff4

--- arch/arm/mach-msm/msm_rq_stats.c
+++ arch/arm/mach-msm/msm_rq_stats.c
@@ -35,6 +35,7 @@
 
 /* only enable on demand if needed */
 static bool load_stats_enabled = false;
+bool rq_data_init_done = false;
 
 #define MAX_LONG_SIZE 24
 #define DEFAULT_RQ_POLL_JIFFIES 1
@@ -61,16 +62,16 @@ static DEFINE_PER_CPU(struct cpu_load_data, cpuload);
 unsigned int get_rq_info(void)
 {
 	unsigned long flags = 0;
-        unsigned int rq = 0;
+	unsigned int rq = 0;
 
-        spin_lock_irqsave(&rq_lock, flags);
+	spin_lock_irqsave(&rq_lock, flags);
 
-        rq = rq_info.rq_avg;
-        rq_info.rq_avg = 0;
+	rq = rq_info.rq_avg;
+	rq_info.rq_avg = 0;
 
-        spin_unlock_irqrestore(&rq_lock, flags);
+	spin_unlock_irqrestore(&rq_lock, flags);
 
-        return rq;
+	return rq;
 }
 EXPORT_SYMBOL(get_rq_info);
 
@@ -178,6 +179,9 @@ unsigned int report_load_at_max_freq(void)
 	struct cpu_load_data *pcpu;
 	unsigned int total_load = 0;
 
+	if (!rq_data_init_done)
+		return 0;
+
 	pcpu = &per_cpu(cpuload, cpu);
 	mutex_lock(&pcpu->cpu_load_mutex);
 	update_average_load(pcpu->cur_freq, cpu);
@@ -194,6 +198,9 @@ static int cpufreq_transition_handler(struct notifier_block *nb,
 	struct cpufreq_freqs *freqs = data;
 	struct cpu_load_data *this_cpu = &per_cpu(cpuload, freqs->cpu);
 	int j;
+
+	if (!rq_data_init_done)
+		return 0;
 
 	switch (val) {
 	case CPUFREQ_POSTCHANGE:
@@ -267,6 +274,10 @@ void enable_rq_load_calc(bool on)
 		load_stats_enabled = on;
 
 		pr_info("Enable rq_stats load calculation %d\n", load_stats_enabled);
+		
+		if (!rq_data_init_done)
+			return;
+
 		if (load_stats_enabled) {
 			// clear data
 			for_each_possible_cpu(cpu) {
@@ -312,7 +323,8 @@ static void def_work_fn(struct work_struct *work)
 	rq_info.def_interval = (unsigned int) diff;
 
 	/* Notify polling threads on change of value */
-	sysfs_notify(rq_info.kobj, NULL, "def_timer_ms");
+	if (rq_data_init_done)
+		sysfs_notify(rq_info.kobj, NULL, "def_timer_ms");
 }
 
 static ssize_t run_queue_avg_show(struct kobject *kobj,
@@ -482,6 +494,7 @@ static int __init msm_rq_stats_init(void)
 					CPUFREQ_POLICY_NOTIFIER);
 	}
 	
+	rq_data_init_done = true;
 	return ret;
 }
 late_initcall(msm_rq_stats_init);
